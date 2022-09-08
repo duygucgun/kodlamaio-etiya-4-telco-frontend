c8e4e83a974d6020a7812bfbc67c10c40dbac837
Duygu Uçgun <duygucgun@gmail.com>
2022-09-08 20:02:58 +0300

    add logout

--- src/app/shared/components/settings-menu/settings-menu.component.ts
+++ src/app/shared/components/settings-menu/settings-menu.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../../core/auth/services/auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings-menu.component.css'],
 })
 export class SettingsMenuComponent implements OnInit {
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
+
+  logOut() {
+    this.authService.logOut();
+  }
 }
